f2c8ff230a869e0bcb445e08d4ad3c1e2dcc2c58
AdityaRaj81 <Connect2RajAditya@gmail.com>
2025-03-02 16:42:57 +0530

    Add store configuration using Redux Toolkit

---
+++ src/store/store.js
@@ -0,0 +1,9 @@
+// src/store.js
+import { configureStore } from "@reduxjs/toolkit";
+import rootReducer from "../store/rootReducer"
+
+export const store = configureStore({
+    reducer: rootReducer,
+
+});
+
